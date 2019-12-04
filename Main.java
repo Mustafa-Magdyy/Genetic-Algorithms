f66c4cbcc8c32315af66fe7209eac3436da4b8f8
Mustafa Magdy <45540037+Mustafa-Magdyy@users.noreply.github.com>
2019-12-04 22:25:08 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main{
-	public static void main(String[] args){
-		double[] x = {0.0, 50.0, 100.0};
-		FuzzySet set = new Triangular("set", x);
-		System.out.println(set.getCentroid());
-	}
-}
